2cbd568317d12ad7da4acf0750366813e80bcade
JalilHashemi <jalil.hashemi@students.fhnw.ch>
2017-05-26 19:59:33 +0200

    questionController was tested

--- src/test/groovy/socrative/AnswerControllerSpec.groovy
+++ src/test/groovy/socrative/AnswerControllerSpec.groovy
@@ -7,9 +7,63 @@ import spock.lang.*
 @Mock(Answer)
 class AnswerControllerSpec extends Specification {
 
+    AnswerController answerController
+
     def setup() {
+        answerController = new AnswerController()
     }
 
     def cleanup() {
+    }
+
+    @Unroll
+    void "test answer's null" () {
+        given:
+        def model = new Answer()
+        when:
+        model.a = null
+
+        then:
+        !model.validate(['a'])
+        model.errors['a'].code == 'nullable'
+    }
+
+    @Unroll
+    void "test answer's blank" () {
+        given:
+        def model = new Answer()
+        when:
+        model.a = ''
+
+        then:
+        !model.validate(['a'])
+    }
+
+    @Unroll
+    void "test answer's max of 50 and min 2 characters" () {
+        given:
+        def model = new Answer()
+        when: 'for a string of 51 characters'
+        String str = 'a' * 51
+        model.a = str
+
+        then: 'name validation fails'
+        !model.validate(['a'])
+        model.errors['a'].code == 'size.toobig'
+
+        when: 'for a string of 1 characters'
+        str = 'a' * 1
+        model.a = str
+
+        then: 'name validation fails'
+        !model.validate(['a'])
+        model.errors['a'].code == 'size.toosmall'
+
+        when: 'for a string of 50 characters'
+        str = 'a' * 50
+        model.a = str
+
+        then: 'name validation passes'
+        model.validate(['a'])
     }
 }
--- src/test/groovy/socrative/QuestionControllerSpec.groovy
+++ src/test/groovy/socrative/QuestionControllerSpec.groovy
@@ -7,10 +7,64 @@ import spock.lang.*
 @Mock(Question)
 class QuestionControllerSpec extends Specification {
 
+    QuestionController answerController
+
     def setup() {
+        answerController = new QuestionController()
     }
 
     def cleanup() {
+    }
+
+    @Unroll
+    void "test question's null" () {
+        given:
+        def model = new Question()
+        when:
+        model.question = null
+
+        then:
+        !model.validate(['question'])
+        model.errors['question'].code == 'nullable'
+    }
+
+    @Unroll
+    void "test question's blank" () {
+        given:
+        def model = new Question()
+        when:
+        model.question = ''
+
+        then:
+        !model.validate(['question'])
+    }
+
+    @Unroll
+    void "test question's max of 500 and min 20 characters" () {
+        given:
+        def model = new Question()
+        when: 'for a string of 501 characters'
+        String str = 'a' * 501
+        model.question = str
+
+        then: 'name validation fails'
+        !model.validate(['question'])
+        model.errors['question'].code == 'size.toobig'
+
+        when: 'for a string of 19 characters'
+        str = 'a' * 19
+        model.question = str
+
+        then: 'name validation fails'
+        !model.validate(['question'])
+        model.errors['question'].code == 'size.toosmall'
+
+        when: 'for a string of 50 characters'
+        str = 'a' * 50
+        model.question = str
+
+        then: 'name validation passes'
+        model.validate(['question'])
     }
 
 }
